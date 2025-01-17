ba3c669115ef4587b30fc9496f2a1f4b5b5dd530
ModMonster <modmonster2060@gmail.com>
2025-01-17 16:15:18 -0500

    Added lang file, unlock condition, and new texture for bonsey

---
+++ jokers/evil_bones.lua
@@ -0,0 +1,46 @@
+SMODS.Joker {
+    key = "evil_bones",
+    rarity = 2,
+    cost = 5,
+    pos = {x = 0, y = 0},
+    atlas = "Jokers",
+    unlocked = false,
+    discovered = false,
+    blueprint_compat = false,
+    eternal_compat = true,
+    perishable_compat = true,
+    calculate = function(self, card, context)
+        if (context.blueprint_card) then return end -- disable blueprint / brainstorm
+        if (context.game_over and G.GAME.chips/G.GAME.blind.chips >= 0.25) then
+            G.hand_text_area.blind_chips:juice_up()
+            G.hand_text_area.game_chips:juice_up()
+            play_sound('tarot1')
+
+            -- code stealed from madness
+            local destructable_jokers = {}
+            for i = 1, #G.jokers.cards do
+                if G.jokers.cards[i] ~= card and not G.jokers.cards[i].ability.eternal then destructable_jokers[#destructable_jokers+1] = G.jokers.cards[i] end
+            end
+            local joker_to_destroy = #destructable_jokers > 0 and pseudorandom_element(destructable_jokers, pseudoseed('evilbones')) or nil
+
+            if joker_to_destroy then
+                joker_to_destroy:start_dissolve({G.C.RED}, nil, 1.6)
+            end
+
+            if (joker_to_destroy) then
+                return {
+                    -- TODO: can we make it not say saved by mr bones?
+                    message = localize("k_saved_ex"),
+                    saved = true,
+                    colour = G.C.RED
+                }
+            end
+        end
+    end,
+    check_for_unlock = function(self, args)
+        -- TODO: is there a better way to do this??
+        if (args.type == "round_win" and G.GAME.chips < G.GAME.blind.chips) then
+            unlock_card(self)
+        end
+    end
+}
---
+++ localization/default.lua
@@ -0,0 +1,20 @@
+return {
+    descriptions = {
+        Joker = {
+            j_mods_evil_bones = {
+                name = "Evil Bones",
+                text = {
+                    "Prevents Death",
+                    "if chips scored",
+                    "are at least {C:attention}25%{}",
+                    "of required chips",
+                    "{s:0.8,C:red,E:2}destroys a random Joker"
+                },
+                unlock = {
+                    "Be saved by",
+                    "{C:attention}Mr. Bones",
+                }
+            }
+        }
+    }
+}
