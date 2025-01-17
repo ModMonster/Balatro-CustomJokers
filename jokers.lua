4e4f5c18eb46da38db3993448d217f735dd1954b
ModMonster <modmonster2060@gmail.com>
2025-01-17 12:27:18 -0500

    Fix the freaking skull guy skeleton code

--- jokers.lua
+++ jokers.lua
@@ -1,2 +1,2 @@
 -- Load all the jokers!
-SMODS.load_file("jokers/skull_and_crossbones")()
+SMODS.load_file("jokers/skull_and_crossbones.lua")()
--- jokers/skull_and_crossbones.lua
+++ jokers/skull_and_crossbones.lua
@@ -3,15 +3,17 @@ SMODS.Joker {
     loc_txt = {
         name = "Skull and Crossbones",
         text = {
-            "Prevents Death if chips scored are at",
-            "least {C:attention}25%{} of required chips",
-            "{S:1.1,C:red,E:2}destroy a random Joker"
+            "Prevents Death",
+            "if chips scored",
+            "are at least {C:attention}25%{}",
+            "of required chips",
+            "{s:0.8,C:red,E:2}destroy a random Joker"
         }
     },
-    rarity = 2
+    rarity = 2,
     pos = {x = 0, y = 0},
     atlas = "Jokers",
     unlocked = false,
     discovered = false,
-    blueprint_compat = false,
+    blueprint_compat = false
 }
